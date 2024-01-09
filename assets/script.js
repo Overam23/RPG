4804bfdfd059d43c5e2648e2d95e622ad27bb8e3
unknown <overam23@gmail.com>
2024-01-09 18:28:43 -0500

    progress

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,45 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+
+
+  var options= {
+  num: [1,2,4,5,6,7,8],
+  upper: ["A", "B", "C", "D", "E"],
+  special: ["!", "@", "#", "$"],
+  lower: ["a", "b", "c", "d", "e"]
+  };
+
+
+
+
+
+  var numQ = prompt("Would you like any numbers? Yes or No ");
+if (numQ == options.num[0-7] );
+window.alert("YASSS!"); 
+
+var uppQ = prompt("Did you want any upppercase letters? Type Yes or No");
+if (uppQ      );
+
+var lowerQ = prompt("Did you want any lowercase letters? Type Yes or No")
+if (lowerQ    );
+
+var specialQ = prompt("Do you care for some special charecters? Type Yes or No")
+if (specialQ   );
+
+
+
+var num = [1,2,4,5,6,7,8]
+var upper = ["A", "B", "C", "D", "E"]
+var special = ["!", "@", "#", "$"]
+var lower = ["a", "b", "c", "d", "e"]
+
+
+
+  
+
+
+var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -21,17 +59,26 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
+
+
+
+
+
+
+
+
+
+
 // window.prompt ("sometext") , "defai;tText");
 //  var person = prompt("Please enter your name");
 //  console.log(person);
 // var con = confirm("message here?")
 // conole.log(con);
 
-var num = [1,2]
-var upper = ["A", "Z"]
-var special = ["$", "%"]
-var lower = ["a", "z"]
 
-var userchoice = [1, 2, "$", "a", "z"]
+
+
+  
+
 
 
